b957c78b809c35a1ae95a101168663d8f7cd2cc5
Gleb <puchkin.2008@mail.ru>
2025-12-01 14:31:34 +0500

    add main app

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ THIRD_PARTY_APPS = [
 # ✅ Добавляй сюда приложения ПОСЛЕ их создания через startapp
 LOCAL_APPS = [
     "apps.accounts",
+    "apps.main",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
